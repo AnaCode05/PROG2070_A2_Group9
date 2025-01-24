4cc06304a59445d4920ecedb769b3fe87718efe0
AnaCode05 <ana.isakov05@gmail.com>
2025-01-24 14:17:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG2070_A2_Group9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
